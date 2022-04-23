a72b975d3922d58c9ee2e47aba9cf68532eb4dd0
dsalinas-id <69652641+dsalinas-id@users.noreply.github.com>
2022-04-23 00:09:34 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,40 @@ module.exports = {
     },
   },
   plugins: [
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-NL35NQJ",
+
+      // Include GTM in development.
+      //
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: true,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      //
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+
+      // Specify optional GTM environment details.
+      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+      dataLayerName: "dataLayer",
+
+      // Name of the event that is triggered
+      // on every Gatsby route change.
+      //
+      // Defaults to gatsby-route-change
+      routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      // Defaults to false
+      enableWebVitalsTracking: true,
+      // Defaults to https://www.googletagmanager.com
+      selfHostedOrigin: "https://ga4shortcuts.com",
+    },
+  },
+]
+  plugins: [
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -130,37 +164,3 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
-plugins: [
-  {
-    resolve: "gatsby-plugin-google-tagmanager",
-    options: {
-      id: "GTM-NL35NQJ",
-
-      // Include GTM in development.
-      //
-      // Defaults to false meaning GTM will only be loaded in production.
-      includeInDevelopment: true,
-
-      // datalayer to be set before GTM is loaded
-      // should be an object or a function that is executed in the browser
-      //
-      // Defaults to null
-      defaultDataLayer: { platform: "gatsby" },
-
-      // Specify optional GTM environment details.
-      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-      dataLayerName: "dataLayer",
-
-      // Name of the event that is triggered
-      // on every Gatsby route change.
-      //
-      // Defaults to gatsby-route-change
-      routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
-      // Defaults to false
-      enableWebVitalsTracking: true,
-      // Defaults to https://www.googletagmanager.com
-      selfHostedOrigin: "https://ga4shortcuts.com",
-    },
-  },
-]
